03afc9feba560f327b36ea53a9599ad6d3c2770c
SushiMan <45275055+SushiMan-o3@users.noreply.github.com>
2025-04-28 00:33:43 -0400

    Add files via upload

--- TicTacToe with Algo.py
+++ TicTacToe with Algo.py
@@ -17,24 +17,33 @@ class PlayDumbassBot(Play):
         print("Let the games begin! You will be playing as X and the bot will be playing as O. \n")
         print(self.board.display())
 
-        while self.board.check_win() is None and self.board.check_draw():
-            move = input("Input your move: ").split(",")
-            self.board.make_move(move[0], move[1], "X")
+        while self.board.check_win() is None and not self.board.check_draw():
+            move = input("Input your move in (x,y): ").split(",")
 
-            print(f"You made the move ({move[0]}, {move[1]}). The current position of the board is: \n \n" + self.board.display() + "\n \n")
+            while not self.board.make_move(move[1], move[0], "X"):
+                move = input("Invalid move, input your move in (x,y): ").split(",")
 
-            move = self.pick_random()
-            self.board.make_move(move[0], move[1], "O")
+            print(f"You made the move ({move[1]}, {move[0]}). The current position of the board is: \n \n" + self.board.display() + "\n \n")
 
-            print(f"The bot the move ({move[0]}, {move[1]}). The current position of the board is: \n \n" + self.board.display() + "\n \n")
+            if self.board.check_win():
+                break 
+
+            bot_move = self.pick_random()
+            self.board.make_move(bot_move[0], bot_move[1], "O")
+
+            print(f"The bot the move ({bot_move[1]}, {bot_move[0]}). The current position of the board is: \n \n" + self.board.display() + "\n \n")
         
-        if self.board.check_draw():
-            print("The following game ended in a draw!")
+        winner = self.board.check_win()
+
+        if winner is not None:
+            print("The winner is " + winner + "!")
         else:
-            print("The winner is " + self.board.check_win() + "!")
+            print("The following game ended in a draw!")
+
 
     def pick_random(self):
-        return random.choice(self.board.get_available_moves())
+        val = random.choice(self.board.get_available_moves())
+        return (val[0] + 1, val[1] + 1)
 
 class PlayProBot(Play):
     def __init__(self):
